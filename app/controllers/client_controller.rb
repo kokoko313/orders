7731f84bbb78ffcde479ad63d6d4329df17fede9
kokoko313 <drabada@gmail.com>
2016-08-30 15:16:12 +0300

    add login/logout string app.layout

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,6 +1,7 @@
 class ClientController < ApplicationController
   before_filter :authenticate_user!, :only => [:index]
+  
   def index
-  @orders = Order.by_user(current_user).all
+    @orders = Order.by_user(current_user).all
   end
 end
