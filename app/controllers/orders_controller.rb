337b8fbff6a98512e5a5a7366adeb7b40264937d
kokoko313 <drabada@gmail.com>
2016-09-04 21:14:13 +0300

    some bootstrap/add orders status update

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,11 +13,24 @@ class OrdersController < ApplicationController
       end    
   end
 
-  def order_params
-      params.require(:order).permit(:status, :product_id, :category_id, :user_id)
-  end 
+  def update
+    set_order
+    respond_to do |format|
+      if @order.update(order_params)
+        format.html { redirect_to passthrough_index_path, notice: 'Product was successfully updated.' }
+        format.json { render :show, status: :ok, location: @product }
+      else
+        format.html { render :edit }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
   def show
+    set_order
+  end
+
+  def edit
     set_order
   end
 
@@ -26,4 +39,9 @@ class OrdersController < ApplicationController
     def set_order
       @order = Order.find(params[:id])
     end
+
+   def order_params
+      params.require(:order).permit(:status, :product_id, :category_id, :user_id)
+  end 
+
 end
